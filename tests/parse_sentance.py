c0c33519d131cb029955ee1c670a895fd527dae1
Alex Drozdov <alex.drozdovs@gmail.com>
2016-03-27 18:11:15 +0300

    parse sentence from cmd line or from test file

--- tests/parse_sentance.py
+++ tests/parse_sentance.py
@@ -2,11 +2,13 @@
 # -*- #coding: utf8 -*-
 
 
+import os
 import sys
 import codecs
 import time
 import json
 import io
+import argparse
 import parser.sentparser
 import parser.graph
 import parser.graph_span
@@ -25,90 +27,68 @@ def timeit_ctx(name):
     print('[{}] finished in {} ms'.format(name, int(elapsedTime * 1000)))
 
 
-sentence = [u'падал', u'прошлогодний', u'снег', u'на', u'теплую', u'землю', u'поля']
-# sentence = [u'мелькнул', u'последний', u'луч', u'жаркого', u'дня']
-# sentence = [u'мелькнул', u'последний', u'яркий', u'луч', u'жаркого', u'дня']
-# sentence = [u'мелькнул', u'последний', u',', u'яркий', u'луч', u'жаркого', u'дня']
-# sentence = [u'падал', u'прошлогодний', u'снег', u'на', u'теплую', u'землю', u',', u'поля']
-# sentence = [u'падал', u'снег']
-# sentence = [u'прошлогодний', u'снег']
-# sentence = [u'она', u'летела']
-# sentence = [u'на', u'поезд']
-# sentence = [u'в', u'комнате']
-# sentence = [u'на', u'солнце']
-# sentence = [u'полная', u'луна', u'медленно', u'плыла', u'на', u'темном', u'ночном', u'небе']
-sentence = [u'полная', u'луна', u'медленно', u'плыла', u'на', u'темном', u'ночном', u'небе']
-sentence = [u'друзья', u'быстро', u'шли', u'по', u'зеленым', u'лугам', u',', u'бескрайним', u'полям']
-sentence = [u'друзья', u'шли', u'по', u'зеленым', u'лугам', u'бескрайним', u'полям']
-# sentence = [u'мои', u'друзья', u'гуляли']
-# sentence = [u'его', u'друзья', u'гуляли']
-# sentence = [u'наши', u'друзья', u'гуляли']
-# sentence = [u'их', u'друзья', u'гуляли']
-# sentence = [u'его', u'и', u'ее',  u'друзья', u'гуляли']
-# sentence = [u'его', u'и', u'ее',  u'друзья', u'пришли', u'на', u'его', u'день', u'рождения']
-# sentence = [u'друзья', u'быстро', u'шли', u'по', u'зеленым', u'лугам', u',', u'бескрайним', u'полям', u'огромной', u'страны']
-# sentence = [u'верные', u'друзья', u'быстро', u'шли', u'по', u'зеленым', u'лугам', u',', u'бескрайним', u'полям', u'огромной', u'страны']
-# sentence = [u'зеленым', u'лугам', u'бескрайним', u'полям']
-# sentence = [u'луна', u'сияла', u'на', u'темном', u'ночном', u'небе']
-# sentence = [u'холод', u'зимы']
-# sentence = [u'медленно', u'падал', u'мокрый', u'снег', u'на', u'землю']
-# sentence = [u'он', u'нашел', u'формулу', u'философского', u'камня']
-# sentence = [u'его', u'следы', u'нашлись', u'в', u'далекой', u'стране']
-# sentence = [u'мое', u'время', u'текло', u'невероятно', u'быстро']
-# sentence = [u'на', u'небе', u'восходило', u'ярко', u'красное', u'солнце']
-# sentence = [u'в', u'весеннем', u'саду', u'цвели', u'ярко', u'красные', u'тюльпаны', u'нежно', u'белые', u'нарциссы', ]
-# sentence = [u'в', u'весеннем', u'саде', u'цвели', u'ярко', u'красные', u'тюльпаны']
-# sentence = [u'в', u'весеннем', u'саде', u'у', u'дома', u'цвели', u'ярко', u'красные', u'тюльпаны']
-# sentence = [u'в', u'весеннем', u'саде', u'у', u'моего', u'дома', u'цвели', u'ярко', u'красные', u'тюльпаны']
-# sentence = [u'на', u'береге', u'синего', u'моря', u'стояла', u'хижина', u'рыбака']
-# sentence = [u'хижина', u'рыбака', u'стояла', u'на', u'береге', u'синего', u'моря']
-# sentence = [u'он', u'сидел', u'на', u'стволе', u'упавшего', u'дерева']
-# sentence = [u'мальчик', u'сидел', u'на', u'стволе', u'упавшего', u'дерева']
-# sentence = [u'наши', u'дела', u'шли']
-# sentence = [u'мы', u'поймали', u'его', u'в', u'парке']
-# sentence = [u'красные', u'цветы']
-# sentence = [u'мальчики', u'и', u'девочки', u'сидели', u'на', u'лавочке']
-sentence = u'мальчики , девочки сидели на лавочке'
-# sentence = u'мама мыла покрашенную раму'
-# sentence = [u'мама', u'мыла', u'покрашенную', u'белой', u'краской', u'раму', u'окна']
-# sentence = [u'я', u'смотрел', u'на', u'березу', u',', u'стоящую', u'под', u'окном', u',', u'покрытую', u'снегом']
-# sentence = [u'косой', u'косой', u'косил', u'косой', u'косой']
-# sentence = u'ярко красный'
+def parse_opts():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sentence')
+    parser.add_argument('-t', '--test')
+    res = parser.parse_args(sys.argv[1:])
 
-if len(sys.argv) > 1:
-    sentence = sys.argv[1].decode('utf8')
+    if res.sentence is None and res.test is None:
+        raise ValueError('neither sentence nor test file specified')
 
-with timeit_ctx('total'):
-    with timeit_ctx('loading database'):
-        tm = parser.sentparser.TokenMapper('./dbs/worddb.db')
-    with timeit_ctx('building parser'):
-        srm = parser.specs.SequenceSpecMatcher(False)
+    if res.sentence is not None and res.test is not None:
+        raise ValueError('only one option sentence or test is allowed')
 
-    with timeit_ctx('tokenizing'):
-        tokens = parser.sentparser.Tokenizer().tokenize(sentence)
+    if res.test is not None and not os.path.exists(res.test):
+        raise ValueError('test file <{0}> not found'.format(res.test))
 
-    with timeit_ctx('mapping word forms'):
-        parsed_sentence = tm.map(tokens)
+    return res
 
-    with timeit_ctx('matching sentences'):
-        matched_sentences = srm.match_sentence(parsed_sentence, most_complete=True)
 
-    for j, sq in enumerate(matched_sentences.get_sequences()):
-        sq.print_sequence()
-        parser.graph.SequenceGraph(img_type='svg').generate(
-            sq,
-            oput.get_output_file('imgs', 'sq-{0}.svg'.format(j))
-        )
-    with io.open(
-        oput.get_output_file('', 'res.json'), 'w', encoding='utf8'
-    ) as jf:
-        s = json.dumps(
-            {
-                'input': sentence,
-                'graph': matched_sentences.export_obj()
-            },
-            jf,
-            ensure_ascii=False,
-            encoding='utf8'
-        )
-        jf.write(s)
+def execute(opts):
+    if opts.sentence is not None:
+        sentence = opts.sentence.decode('utf8')
+    elif opts.test is not None:
+        with open(opts.test) as f:
+            data = json.load(f)
+            sentence = data['input']
+
+    with timeit_ctx('total'):
+        with timeit_ctx('loading database'):
+            tm = parser.sentparser.TokenMapper('./dbs/worddb.db')
+        with timeit_ctx('building parser'):
+            srm = parser.specs.SequenceSpecMatcher(False)
+
+        with timeit_ctx('tokenizing'):
+            tokens = parser.sentparser.Tokenizer().tokenize(sentence)
+
+        with timeit_ctx('mapping word forms'):
+            parsed_sentence = tm.map(tokens)
+
+        with timeit_ctx('matching sentences'):
+            matched_sentences = srm.match_sentence(parsed_sentence, most_complete=True)
+
+        for j, sq in enumerate(matched_sentences.get_sequences()):
+            sq.print_sequence()
+            parser.graph.SequenceGraph(img_type='svg').generate(
+                sq,
+                oput.get_output_file('imgs', 'sq-{0}.svg'.format(j))
+            )
+        with io.open(
+            oput.get_output_file('', 'res.json'), 'w', encoding='utf8'
+        ) as jf:
+            s = json.dumps(
+                {
+                    'input': sentence,
+                    'graph': matched_sentences.export_obj()
+                },
+                jf,
+                ensure_ascii=False,
+                encoding='utf8'
+            )
+            jf.write(s)
+
+
+if __name__ == '__main__':
+    opts = parse_opts()
+    execute(opts)
