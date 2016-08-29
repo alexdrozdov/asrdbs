6257c5f59206f31fcf3cd62b1b218865f4fcccb7
Alex Drozdov <alex.drozdovs@gmail.com>
2016-08-29 08:55:46 +0300

    format __init__

--- parser/templates/__init__.py
+++ parser/templates/__init__.py
@@ -11,15 +11,19 @@ def load_templates():
     mpath = mpath[mpath.rfind('parser'):].replace('/', '.').replace('\\', '.')
     local_path = os.path.dirname(os.path.realpath(__file__))
     __all__ = [m for m in set(
-            [m.replace('.pyc', '.py').replace('.py', '') for m in os.listdir(local_path)]
-        ) if m != '__init__' and '.' not in m]
+        [m.replace('.pyc', '.py').replace('.py', '')
+            for m in os.listdir(local_path)]
+    ) if m != '__init__' and '.' not in m]
     clss = []
     for mname in __all__:
         obj = __import__(mpath + '.' + mname, globals(), locals(), mname)
         clss.extend(
             [c[1] for c in inspect.getmembers(
-                    obj,
-                    lambda c: inspect.isclass(c) and c is not parser.templates.common.SpecTemplate and issubclass(c, parser.templates.common.SpecTemplate)
-                )]
+                obj,
+                lambda c:
+                    inspect.isclass(c) and
+                    c is not parser.templates.common.SpecTemplate and
+                    issubclass(c, parser.templates.common.SpecTemplate)
+            )]
         )
     return clss
