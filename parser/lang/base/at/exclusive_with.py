390eefdc7d25b5eaaaa31b154efb37fa22d535dc
Alex Drozdov <alex.drozdovs@gmail.com>
2016-11-03 21:11:26 +0300

    introduce grammar selectors

--- parser/lang/base/at/exclusive_with.py
+++ parser/lang/base/at/exclusive_with.py
@@ -4,6 +4,6 @@ from parser.lang.base.rules.defs import AnchorSpecs
 
 @parser.spare.at(name='exclusive-with', namespace='specs')
 @parser.spare.constructable
-def exclusive_with(self, body, *args, **kwargs):
+def exclusive_with(body, *args, **kwargs):
     exw_v = body.pop('@exclusive-with')[0]
     body["exclusive-with"] = AnchorSpecs().ExclusiveWith(exw_v)
