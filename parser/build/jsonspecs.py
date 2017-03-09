c75940c7ee1b8b1e211724f83b997cdf842d0382
Alex Drozdov <alex.drozdovs@gmail.com>
2017-03-10 02:14:33 +0300

    introduce declare/implement pattern for structure

--- parser/build/jsonspecs.py
+++ parser/build/jsonspecs.py
@@ -57,12 +57,118 @@ class _PreCompiler(object):
         ), ]
 
 
+class SpecDecl(object):
+    def __init__(self, name):
+        self.__name = name
+
+    def name(self):
+        return self.__name
+
+    def declared(self):
+        raise RuntimeError('not implemented')
+
+    def format(self, fmt):
+        raise RuntimeError('not implemented')
+
+
+class SingleImplSpecDecl(SpecDecl):
+    def __init__(self, impl, filename):
+        super().__init__(impl['name'])
+        self.__impl = impl
+        self.__filename = filename
+
+    def get_origin(self):
+        return self.__filename
+
+    def declared(self):
+        return True
+
+    def format(self, fmt):
+        if fmt == 'dict':
+            return self.__format_dict()
+        raise ValueError('Unsupported format {0}'.format(fmt))
+
+    def __format_dict(self):
+        return self.__impl
+
+
+class MultiImplSpecDecl(SpecDecl):
+    def __init__(self, name, filename=None, options=None):
+        super().__init__(name)
+        self.__filename = filename
+        self.__options = options
+        self.__implementations = {}
+
+    def get_origin(self):
+        return self.__filename
+
+    def declare(self, filename, options=None):
+        self.__filename = filename
+        self.__options = options
+
+    def declared(self):
+        return self.__filename is not None
+
+    def add_implementation(self, impl):
+        name = impl['name']
+        if name in self.__implementations:
+            raise ValueError(
+                'Multiple impementation {0} for declaration {1}'.format(
+                    name, self.__name))
+        self.__implementations[name] = impl
+
+    def format(self, fmt):
+        if fmt == 'dict':
+            return self.__format_dict()
+        raise ValueError('Unsupported format {0}'.format(fmt))
+
+    def __mkitem(self, name, content):
+        content = copy.deepcopy(content)
+        content.pop('name')
+        content['@id'] = name
+        content['@inherit'] = ['once', ]
+        return content
+
+    def __format_dict(self):
+        uniq_items = [self.__mkitem(k, v)
+                      for k, v in self.__implementations.items()]
+        return {
+            'name': self.name(),
+            'entries': [
+                {
+                    '@id': self.name(),
+                    '@inherit': ['once', ],
+                    'uniq-items': uniq_items,
+                }
+            ]
+        }
+
+
+class FileScope(object):
+    def __init__(self, name, entries):
+        self.__name = name
+        self.__entries = entries
+
+    def name(self):
+        return self.__name
+
+    def __iter__(self):
+        return iter(self.__entries)
+
+
+class GlobalScope(dict):
+    pass
+
+
 class _Specs(object):
     def __init__(self):
         self.__specs = {}
-        self.__load_specs()
+        self.__global_scope = GlobalScope()
+        self.__files = []
+        self.__scan_files()
+        self.__precompile_files()
 
-    def __load_specs(self):
+    def __scan_files(self):
         cfg = common.config.Config()
         if not cfg.exists('/parser/specdefs'):
             return
@@ -78,11 +184,72 @@ class _Specs(object):
                 self.__load_file(fullname)
 
     def __load_file(self, filename):
-        scope = PreprocessScope()
         mj = common.multijson.MultiJsonFile(filename)
-        for j in mj.dicts():
-            res = PreCompiler().compile(copy.deepcopy(j), scope=scope)
-            scope.add_specs(res, source=copy.deepcopy(j))
+        local_file_dicts = self.__combine_implementations(filename, mj.dicts())
+        scope = FileScope(filename, local_file_dicts)
+        self.__files.append(scope)
+
+    def __combine_implementations(self, filename, dicts):
+        res = []
+        for j in dicts:
+            if '@declare' in j:
+                res.append(self.__declare(j['@declare'], filename))
+            elif '@implement' in j:
+                self.__add_implementation(j, filename)
+            else:
+                res.append(self.__declare_single(j, filename))
+        return res
+
+    def __declare(self, decl_name, filename, options=None):
+        if options is None:
+            options = {}
+
+        declaration = self.__global_scope.get(decl_name)
+        if declaration is None:
+            declaration = MultiImplSpecDecl(decl_name, filename, options)
+            self.__global_scope[decl_name] = declaration
+        elif not declaration.declared():
+            declaration.declare(filename, options=options)
+        else:
+            raise ValueError(
+                'Duplicate declaration for spec {0}. Declared in {1}'.format(
+                    decl_name, declaration.get_origin()
+                )
+            )
+        return declaration
+
+    def __declare_single(self, j, filename):
+        decl_name = j['name']
+        declaration = self.__global_scope.get(decl_name)
+        if declaration is not None:
+            raise ValueError(
+                'Single-implementation for spec {0}. Declared in {1}'.format(
+                    decl_name, declaration.get_origin()
+                )
+            )
+        declaration = SingleImplSpecDecl(j, filename)
+        self.__global_scope[decl_name] = declaration
+        return declaration
+
+    def __add_implementation(self, j, filename):
+        decl_name = j.pop('@implement')
+        o = self.__global_scope.get(decl_name)
+        if o is None:
+            o = MultiImplSpecDecl(decl_name, options=None)
+            self.__global_scope[decl_name] = o
+
+        o.add_implementation(j)
+
+    def __precompile_files(self):
+        for f in self.__files:
+            self.__precompile_file(f)
+
+    def __precompile_file(self, f):
+        scope = PreprocessScope()
+        for j in f:
+            obj = j.format('dict')
+            res = PreCompiler().compile(copy.deepcopy(obj), scope=scope)
+            scope.add_specs(res, source=copy.deepcopy(obj))
             self.__add_specs(res)
 
     def __add_specs(self, specs):
