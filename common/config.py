de5ccb4eede6cddfb763a4e2e3b0d0ad197c7ccd
Alex Drozdov <alex.drozdovs@gmail.com>
2017-06-11 16:39:58 +0300

    load config from multiple files, support command line config override

--- common/config.py
+++ common/config.py
@@ -15,26 +15,109 @@ def singleton(class_):
     return getinstance
 
 
+def v2bool(s):
+    if isinstance(s, bool):
+        return s
+    if isinstance(s, str):
+        return s.lower() in ('true', 'yes', 'y', 't', '1')
+    return bool(s)
+
+
 class AppConfig(object):
-    def __init__(self, filename=None, obj=None):
-        if filename is not None:
-            assert obj is None
-            with open(filename) as f:
-                obj = json.load(f)
+    def __init__(self, filenames=None, objs=None, override_args=None):
+        objs = self.__to_obj_list(filenames, objs)
+        obj = self.__combine_objs(objs)
+        self.__override_args(override_args, obj)
         self.__obj = obj
 
-    def register_unit(self, name, cfg=None):
-        if name in self.__obj:
-            return
-        if cfg is None:
-            cfg = {}
-            self.__obj[name] = cfg
+    def __to_obj_list(self, filenames, objs):
+        assert filenames is None or objs is None
+        if filenames is not None:
+            objs = []
+            if isinstance(filenames, str):
+                filenames = [filenames, ]
+            for fn in filenames:
+                with open(fn) as f:
+                    objs.append(json.load(f))
+        if not isinstance(objs, list):
+            objs = [objs, ]
+        return objs
+
+    def __combine_objs(self, objs):
+        obj = objs[0]
+        objs = objs[1:]
+        for o in objs:
+            self.__combine_obj(obj, o)
+        return obj
+
+    def __combine_obj(self, obj, o):
+        pathes = self.__dict_to_pathes_values(o)
+        for p, v in pathes:
+            vv = self.__xpath_get(p, obj=obj)
+            if vv is None:
+                self.__xpath_set(p, v, obj=obj)
+            elif isinstance(vv, list) and isinstance(v, list):
+                vv.extend(v)
+            elif isinstance(vv, list) and not isinstance(v, list):
+                vv.append(v)
+            else:
+                raise ValueError('Couldn`t set path {0}: {1}'.format(p, v))
+
+    def __dict_to_pathes_values(self, d):
+        return list(self.__traverse_dicts('', d))
+
+    def __traverse_dicts(self, prefix, d):
+        for k, v in d.items():
+            if isinstance(v, dict):
+                for kk, vv in self.__traverse_dicts(prefix + '/' + k, v):
+                    yield (kk, vv)
+            else:
+                yield (prefix + '/' + k, v)
+
+    def __override_args(self, args, obj=None):
+        for a in args:
+            path, value, action = self.__parse_arg(a)
+            value = self.__cast_value(path, value, obj=obj)
+            if action == 'set':
+                self.__xpath_set(path, value, obj=obj)
+            elif action == 'append':
+                self.__xpath_append(path, value, obj=obj)
+            else:
+                raise ValueError('Unknown action for {0}'.format(a))
+
+    def __parse_arg(self, a):
+        if '+=' in a:
+            path, value = a.split('+=')
+            return path, value, 'append'
+        if '-=' in a:
+            path, value = a.split('+=')
+            return path, value, 'delete'
+        if '=' in a:
+            l = a.split('=')
+            path = l[0]
+            value = l[1] if len(l) == 2 else None
+            return path, value, 'set'
+        return None, None, 'none'
+
+    def __cast_value(self, path, value, obj=None):
+        v = self.__xpath_get(path, obj=obj)
+        if v is None:
+            print('Warning: casting to unknown value at {0}'.format(path))
+            return value
+        if isinstance(v, bool):
+            return v2bool(value)
+        if isinstance(v, int):
+            return int(value)
+        return value
 
     def exists(self, path):
         return self.__xpath_get(path) is not None
 
-    def __xpath_get(self, path):
-        elem = self.__obj
+    def __xpath_get(self, path, obj=None):
+        if obj is not None:
+            elem = obj
+        else:
+            elem = self.__obj
         try:
             for x in path.strip("/").split("/"):
                 try:
@@ -46,8 +129,18 @@ class AppConfig(object):
             return None
         return elem
 
-    def __xpath_set(self, path, value):
-        elem = self.__obj
+    def __xpath_append(self, path, value, obj=None):
+        l = self.__xpath_get(path, obj=obj)
+        if not isinstance(l, list):
+            raise ValueError(
+                'Couldnt append non-list {0}: {1}'.format(path, value))
+        l.append(value)
+
+    def __xpath_set(self, path, value, obj=None):
+        if obj is not None:
+            elem = obj
+        else:
+            elem = self.__obj
         path = path.strip("/").split("/")
         base_path = path[0:-1]
         last_name = path[-1]
@@ -57,7 +150,11 @@ class AppConfig(object):
                     x = int(x)
                     elem = elem[x]
                 except ValueError:
-                    elem = elem.get(x)
+                    if x in elem:
+                        elem = elem.get(x)
+                    else:
+                        elem[x] = {}
+                        elem = elem[x]
             try:
                 x = int(last_name)
                 elem[x] = value
--- tests/parse_sentance.py
+++ tests/parse_sentance.py
@@ -36,10 +36,13 @@ def timeit_ctx(name):
 
 def parse_opts():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', action='append', default=[])
+    parser.add_argument('-o', '--option', action='append', default=[])
+
     parser.add_argument('-s', '--sentence')
     parser.add_argument('-t', '--test')
-    parser.add_argument('-p', '--primary')
     parser.add_argument('-m', '--make-test', action='store_true', default=False)
+
     parser.add_argument(
         '-b', '--build-only',
         action='store_true', default=False
@@ -48,6 +51,9 @@ def parse_opts():
     parser.add_argument('--no-selectors', action='store_true', default=False)
     parser.add_argument('--no-database', action='store_true', default=False)
     res = parser.parse_args(sys.argv[1:])
+
+    if not res.config:
+        raise ValueError('no config file provided')
 
     if res.sentence is None and res.test is None:
         raise ValueError('neither sentence nor test file specified')
@@ -61,13 +67,18 @@ def parse_opts():
     return res
 
 
-def execute(opts):
+def get_sentence(opts):
     if opts.sentence is not None:
         sentence = opts.sentence
     elif opts.test is not None:
         with open(opts.test) as f:
             data = json.load(f)
             sentence = data['input']
+    return sentence
+
+
+def execute(opts):
+    sentence = get_sentence(opts)
 
     with timeit_ctx('total'):
         if not opts.no_database:
@@ -95,64 +106,7 @@ def execute(opts):
 
 
 if __name__ == '__main__':
-    cfg = common.config.Config(
-        obj={
-            'app': {
-                'modules': [
-                    'parser',
-                    'worddb',
-                ],
-            },
-            'parser': {
-                'compiler': {
-                    'max-static-include-level': 1,
-                    'force-static-include': False,
-                    'force-dynamic-include': False
-                },
-                'specdefs': ['parser/volume/ru_RU/structure', ],
-                'linkdefs': ['ru_RU/enchanced/linkdefs', ],
-                'selectors': ['parser/volume/ru_RU/selectors', ],
-                'templates': [
-                    'parser/templates',
-                    'parser/lang/ru_RU/enchanced/templates/selectors'
-                ],
-                'props': ['parser/volume/ru_RU/properties', ],
-                'io': {
-                    'jinja2': {
-                        'templates': ['parser/volume/io/jinja2/templates', ],
-                        'styles': 'parser/volume/io/jinja2/styles',
-                        'fmtmap': {
-                            'graph': 'graph.dot.tmpl'
-                        }
-                    }
-                },
-                'debug': {
-                    'src': {
-                        'svg': False,
-                        'json': True,
-                        'path': 'preprocessor'
-                    },
-                    'structure': {
-                        'svg': False,
-                        'json': False,
-                        'path': 'structure'
-                    },
-                    'selectors': {
-                        'svg': False,
-                        'json': False,
-                        'path': 'selectors'
-                    },
-                    'sequences': {
-                        'svg': True,
-                        'json': True,
-                        'str': True,
-                        'path': 'sequences'
-                    }
-                }
-            },
-        }
-    )
-
     opts = parse_opts()
-    cfg['/parser/primary'] = opts.primary
+    cfg = common.config.Config(filenames=opts.config, override_args=opts.option)
+
     execute(opts)
